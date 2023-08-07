7df9f38c6f5e39cfdd3102f5bce5b6ad62417867
Adam <adamvenord7@gmail.com>
2023-08-07 15:30:33 -0600

    fixed music

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,7 +32,7 @@ import {
   devpoint,
   upwork,
   semantic,
-
+  music,
   spiderwhite,
 } from '../assets';
 
@@ -68,7 +68,10 @@ const services = [
     title: 'Photography',
     icon: polaroid,
   },
-
+  {
+    title:'Music',
+    icon: music,
+  },
 ];
 
 const photos = [
